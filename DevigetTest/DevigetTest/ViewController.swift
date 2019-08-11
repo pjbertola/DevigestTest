24ebcfeaad2ca21e3e3fb670217cefdf92faf3e7
Pablo J. Bertola <pbertola@belatrixsf.com>
2019-08-11 18:34:18 -0300

    add project

---
+++ DevigetTest/DevigetTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DevigetTest
+//
+//  Created by Pablo Javier Bertola on 11/08/2019.
+//  Copyright © 2019 Pablo Javier Bertola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
