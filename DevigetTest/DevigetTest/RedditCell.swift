5ddbb4a775ed80c8b5fdf92b6addeeb251971dd4
Pablo J. Bertola <pbertola@belatrixsf.com>
2019-08-11 19:12:17 -0300

    Add RedditCell

---
+++ DevigetTest/DevigetTest/RedditCell.swift
@@ -0,0 +1,14 @@
+//
+//  RedditCell.swift
+//  DevigetTest
+//
+//  Created by Pablo Javier Bertola on 11/08/2019.
+//  Copyright © 2019 Pablo Javier Bertola. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RedditCell: UITableViewCell{
+    
+}
