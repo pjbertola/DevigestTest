fbac023fce3dc32b8d6f52f1d2febb8565f38806
Pablo J. Bertola <pbertola@belatrixsf.com>
2019-08-18 17:24:30 -0300

    add networking

--- DevigetTest/DevigetTest/Networking/NetworkingManager.swift
+++ DevigetTest/DevigetTest/Networking/NetworkingManager.swift
@@ -7,3 +7,68 @@
 //
 
 import Foundation
+
+typealias SuccessHandler = ((_ json: [String: Any]?) -> Void)
+typealias ErrorHandler = ((_ error: Error?) -> Void)
+
+class NetworkingManager {
+    
+    
+    static let shared = NetworkingManager()
+    
+    /**
+     Private initialization to ensure just one instance is created.
+     */
+    fileprivate init() { }
+
+        
+    func getTopReddit(limit: String?,
+                      before: String?,
+                      after: String?,
+                      successHandler: @escaping SuccessHandler,
+                      errorHandler: @escaping ErrorHandler){
+        
+        var urlStr = "https://www.reddit.com/top.json"
+        var params = "?"
+        if let lim = limit {
+            params += "limit=\(lim)&"
+        }
+        if let bef = before {
+            params += "before=\(bef)&"
+        }
+        if let aft = after {
+            params += "after=\(aft)"
+        }
+        urlStr += params
+        guard let myUrl = URL(string: urlStr) else {
+            errorHandler(nil)
+            return
+        }
+        var request = URLRequest(url: myUrl);
+        request.httpMethod = "GET";
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+                if error != nil {
+                    print(error!.localizedDescription)
+                    DispatchQueue.main.async {
+                        errorHandler(error)
+                    }
+                    return
+                }
+            
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String: Any]
+
+                    print("success)")
+                    successHandler(json)
+
+            } catch {
+                print(error)
+                errorHandler(error)
+
+            }
+        }
+        task.resume()
+        
+    }
+    
+}
