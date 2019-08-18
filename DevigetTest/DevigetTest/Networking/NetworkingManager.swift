0a4350bd244902956282a71d85d54443742094ed
Pablo J. Bertola <pbertola@belatrixsf.com>
2019-08-18 17:23:22 -0300

    Add Networking

---
+++ DevigetTest/DevigetTest/Networking/NetworkingManager.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkingManager.swift
+//  DevigetTest
+//
+//  Created by Pablo Javier Bertola on 18/08/2019.
+//  Copyright © 2019 Pablo Javier Bertola. All rights reserved.
+//
+
+import Foundation
